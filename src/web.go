bdbc3c220e87999e52e653fdf1cb942a8f726f5b
Alexander von Renteln <alexander.vonrenteln@advantest.com>
2016-12-04 12:03:17 +0100

    add web.go

---
+++ src/web.go
@@ -0,0 +1,23 @@
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const port = "3000"
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world\n"))
+}
+
+func main() {
+
+	http.HandleFunc("/", Handler)
+	http.HandleFunc("/health", Handler)
+
+	fmt.Println("listening on " + port)
+	http.ListenAndServe(":"+port, nil)
+}
+
